c00e2d80bd5187211b7d8775c979bf32906bec76
Mateusz Ryndak <kontakt@mryndak.pl>
2021-06-23 14:38:03 +0200

    Initial project

---
+++ src/main/java/com/kodilla/ecommerce/dto/ProductDto.java
@@ -0,0 +1,18 @@
+package com.kodilla.ecommerce.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+public class ProductDto {
+
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private Long groupId;
+
+}
