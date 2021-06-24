f059e5288f62420e08493c5302f38ae19f4a0183
mc139 <maciejandrzejcieslik@gmail.com>
2021-06-24 15:19:17 +0100

    GroupController and GroupDto with test data added

---
+++ src/main/java/com/kodilla/ecommerce/controller/GroupController.java
@@ -0,0 +1,23 @@
+package com.kodilla.ecommerce.controller;
+
+import com.kodilla.ecommerce.dto.GroupDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/v1/example")
+@RequiredArgsConstructor
+public class GroupController {
+
+    @GetMapping
+    public GroupDto groupGet() {
+        GroupDto groupDto = new GroupDto();
+        groupDto.setId(1);
+        groupDto.setName("Test Group");
+        return groupDto;
+    }
+
+}
