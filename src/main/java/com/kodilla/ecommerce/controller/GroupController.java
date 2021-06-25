df3ea1cc0ab0062d7cd0677d3b8adfbd154b251b
mc139 <maciejandrzejcieslik@gmail.com>
2021-06-25 14:00:02 +0100

    GroupController fixed

--- src/main/java/com/kodilla/ecommerce/controller/GroupController.java
+++ src/main/java/com/kodilla/ecommerce/controller/GroupController.java
@@ -2,22 +2,59 @@ package com.kodilla.ecommerce.controller;
 
 import com.kodilla.ecommerce.dto.GroupDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @RestController
-@RequestMapping("/v1/example")
+@RequestMapping("/v1/groups")
 @RequiredArgsConstructor
 public class GroupController {
 
-    @GetMapping
-    public GroupDto groupGet() {
+    @RequestMapping(method = RequestMethod.GET, value = "getGroups")
+    public List<GroupDto> getGroups() {
+        List<GroupDto> rtn = new ArrayList<>();
+        GroupDto testGroupDto1 = new GroupDto();
+        GroupDto testGroupDto2 = new GroupDto();
+        GroupDto testGroupDto3 = new GroupDto();
+        testGroupDto1.setId(1);
+        testGroupDto2.setId(2);
+        testGroupDto3.setId(3);
+        testGroupDto1.setName("Test Groups 1");
+        testGroupDto2.setName("Test Groups 2");
+        testGroupDto3.setName("Test Groups 3");
+        rtn.add(testGroupDto1);
+        rtn.add(testGroupDto2);
+        rtn.add(testGroupDto3);
+        return rtn;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "getGroup")
+    public GroupDto getGroup(@RequestParam Integer groupId) {
         GroupDto groupDto = new GroupDto();
+        groupDto.setName("Test One Group");
         groupDto.setId(1);
-        groupDto.setName("Test Group");
         return groupDto;
     }
 
+    @RequestMapping(method = RequestMethod.POST, value = "createGroup", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void createGroup(@RequestBody GroupDto groupDto) {
+
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "deleteGroup")
+    public void deleteGroup(@RequestParam Integer groupId) {
+
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "updateGroup")
+    public GroupDto updateGroup(@RequestBody GroupDto groupDto) {
+        GroupDto stub = new GroupDto();
+        stub.setId(groupDto.getId());
+        stub.setName(groupDto.getName());
+        return stub;
+    }
 }
--- src/main/java/com/kodilla/ecommerce/dto/GroupDto.java
+++ src/main/java/com/kodilla/ecommerce/dto/GroupDto.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 @Data
 public class GroupDto {
+
     private int id;
     private String name;
 
