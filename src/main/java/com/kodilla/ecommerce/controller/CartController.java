ec07548e7ce3c368ee7b4be4e3ca8b1ae091943c
AdrianSkrzypulec <adrian.skrzypulec@gmail.com>
2021-06-28 22:40:31 +0200

    JDP210601-3

--- src/main/java/com/kodilla/ecommerce/controller/CartController.java
+++ src/main/java/com/kodilla/ecommerce/controller/CartController.java
@@ -2,16 +2,18 @@ package com.kodilla.ecommerce.controller;
 import com.kodilla.ecommerce.dto.CartDto;
 import com.kodilla.ecommerce.dto.ExampleDto;
 import com.kodilla.ecommerce.dto.ProductDto;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
 @RestController
 @RequestMapping(value="/v1/cart")
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class CartController {
 
 //    private final CartService cartService;
@@ -19,15 +21,14 @@ public class CartController {
 //    private final ProductMapper productMapper;
 //    private final OrderMapper orderMapper;
 
-    //        utworzenie pustego koszyka
-    @PostMapping(value = "createCart")
-    public void createCart() {
+
+    @PostMapping
+    public void createCart(@RequestBody final CartDto cartDto) {
         System.out.println("Uworzono koszyk");
     }
 
-    //        pobranie elementów z pustego koszyka
-    @GetMapping(value = "getCart")
-    public CartDto getCart(@RequestParam long id) {
+    @GetMapping(value = "/{id}")
+    public CartDto getCart(@PathVariable final Long id) {
         System.out.println("Koszyk został pobrany");
         return CartDto.builder()
                 .id(1L)
@@ -37,23 +38,19 @@ public class CartController {
                 .build();
     }
 
-    //        dodanie elementów (produktów) do koszyka
-    @PutMapping(value = "addProduct")
-    public List<ProductDto> addProduct(@RequestParam long cartId, @RequestParam long productId) {
+    @PutMapping(value = "/{cartId}/{productId}")
+    public List<ProductDto> addProduct(@PathVariable long cartId, @PathVariable long productId) {
         System.out.println("Produkt dodano do koszyka");
         return null;
     }
 
-    //        usunięcie konkretnego produktu z koszyka
-    @DeleteMapping(value = "removeProduct")
-    public void removeProduct(@RequestParam long cartId, @RequestParam long productId) {
+    @DeleteMapping (value = "/{cartId}/{productId}")
+    public void removeProduct(@PathVariable long cartId, @PathVariable long productId) {
         System.out.println("Produkt został usunięty z koszyka");
     }
 
-    //        utworzenie zamówienia na podstawie koszyka
     @PostMapping(value = "makeOrder")
-    public ExampleDto makeOrder(@RequestParam long id) { //finalnie OrderDto
+    public void makeOrder(@RequestBody final ExampleDto orderDto) { //finalnie OrderDto
         System.out.println("Utworzono zamówienie i usunięto koszyk");
-        return null;
     }
 }
