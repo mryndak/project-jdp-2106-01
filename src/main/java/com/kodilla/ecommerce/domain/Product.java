7ae3c6c12636c59078d4b3ce536fca3dedf81473
barteknat <73423811+barteknat@users.noreply.github.com>
2021-06-28 21:34:23 +0200

    JDP210601-12: added Product entity (#6)
    Co-authored-by: Bartosz Natora <bartek@MacBook-Pro-Bartosz.local>

---
+++ src/main/java/com/kodilla/ecommerce/domain/Product.java
@@ -0,0 +1,43 @@
+package com.kodilla.ecommerce.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+//import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Entity(name = "Products")
+public class Product {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private BigDecimal price;
+
+    private Long groupId;
+
+//    @ManyToOne
+//    @JoinColumn(name = "ORDERITEM_ID")
+//    private OrderItem orderItem;
+//
+//    @ManyToOne
+//    @JoinColumn(name = "GROUP_ID")
+//    private Group group;
+//
+//    @ManyToMany(
+//            cascade = CascadeType.ALL,
+//            targetEntity = Cart.class,
+//            mappedBy = "carts"
+//    )
+//    private List<Cart> carts = new ArrayList<>();
+}
