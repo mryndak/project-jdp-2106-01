3f4ac15a207d67c24b51cf521e76537540c10759
Michal Swigost <mike@data.pl>
2021-06-29 11:05:47 +0200

    JDP210601-5: UserController to review

--- src/main/java/com/kodilla/ecommerce/controller/UserController.java
+++ src/main/java/com/kodilla/ecommerce/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.kodilla.ecommerce.controller;
 
 import com.kodilla.ecommerce.dto.UserDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -14,11 +17,22 @@ public class UserController {
     //private final UserMapper userMapper;
 
     @PostMapping
-    public UserDto create(@RequestBody UserDto userDto){
+    public ResponseEntity createUser(@RequestBody UserDto userDto){
         System.out.println("id: " + userDto.getId() + ", name: " + userDto.getUsername() + " was created");
-        return null;
+//        userService.addUser(userMapper.DtoToEntity(userDto)) mapper powinien być static
+        return new ResponseEntity(HttpStatus.CREATED);
     }
-
-
+    @PutMapping
+    public ResponseEntity blockUser(@RequestBody String id){
+//        userService.blockUserById(Long.parseLong(id));
+        return new ResponseEntity(HttpStatus.OK);
+    }
+    @GetMapping
+    public ResponseEntity userLogin(@RequestBody Map<String, String> creds){
+        String username = creds.get("username");
+        String password = creds.get("password");
+//        userService.login(username, password); metoda login przypisuje token działający określonu czas - połączone w bazie po kluczu obcym iduser
+        return new ResponseEntity(HttpStatus.OK);
+    }
 
 }
