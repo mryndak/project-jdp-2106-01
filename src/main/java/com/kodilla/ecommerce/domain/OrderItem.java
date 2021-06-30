6f39bd1f3403bb0bfac3cffc4cfc8d2cf62e58da
pk <abazel@localhost.localdomain>
2021-06-30 11:33:35 +0200

    JDP210601-11 Order Entity implementing

--- src/main/java/com/kodilla/ecommerce/domain/OrderItem.java
+++ src/main/java/com/kodilla/ecommerce/domain/OrderItem.java
@@ -1,5 +1,32 @@
 package com.kodilla.ecommerce.domain;
 
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderItem {
-    //ToDo
+
+    private Long id;
+    private Order order;
+
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name = "ORDER_ID", unique = true)
+    public Long getId() {
+        return id;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "ORDER_ID")
+    public Order getOrder() {
+        return order;
+    }
 }
