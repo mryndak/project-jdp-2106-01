2cd3e7237720870b79db0626015e1214a8e2533c
Michal Swigost <mike@data.pl>
2021-07-01 12:05:41 +0200

    JDP210601-5: delete Mapper from Controller

--- src/main/java/com/kodilla/ecommerce/controller/UserController.java
+++ src/main/java/com/kodilla/ecommerce/controller/UserController.java
@@ -12,27 +12,26 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @RestController
 @RequestMapping(value = "/v1/user", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
 public class UserController {
-
     //private final UserService userService;
-    //private final UserMapper userMapper;
 
     @PostMapping
-    public ResponseEntity createUser(@RequestBody UserDto userDto){
-        System.out.println("id: " + userDto.getId() + ", name: " + userDto.getUsername() + " was created");
+    public ResponseEntity createUser(@RequestBody UserDto userDto) {
+        System.out.println("id: " + userDto.getId() + ", name: " + userDto.getUserName() + " was created");
 //        userService.addUser(userMapper.DtoToEntity(userDto)) mapper powinien być static
         return new ResponseEntity(HttpStatus.CREATED);
     }
+
     @PutMapping
-    public ResponseEntity blockUser(@RequestBody String id){
+    public ResponseEntity blockUser(@RequestBody String id) {
 //        userService.blockUserById(Long.parseLong(id));
         return new ResponseEntity(HttpStatus.OK);
     }
+
     @GetMapping
-    public ResponseEntity userLogin(@RequestBody Map<String, String> creds){
+    public ResponseEntity userLogin(@RequestBody Map<String, String> creds) {
         String username = creds.get("username");
         String password = creds.get("password");
 //        userService.login(username, password); metoda login przypisuje token działający określonu czas - połączone w bazie po kluczu obcym iduser
         return new ResponseEntity(HttpStatus.OK);
     }
-
 }
