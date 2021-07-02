34aaa11914a99dc5d7860dcadedc13525d252c4d
mc139 <maciejandrzejcieslik@gmail.com>
2021-07-02 14:24:47 +0100

    Group entity and GroupDto modified according to comments

--- src/main/java/com/kodilla/ecommerce/domain/Group.java
+++ src/main/java/com/kodilla/ecommerce/domain/Group.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 
@@ -15,6 +16,7 @@ import javax.persistence.Id;
 public class Group {
 
     @Id
+    @GeneratedValue
     private Long id;
 
     private String groupName;
