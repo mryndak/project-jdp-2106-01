39c9ad1e945563d9779707382105549b426fb7f2
piotrek-kuder <kuder.piotr@o2.pl>
2021-07-07 12:42:00 +0200

    correcting OrderItem column names

--- src/main/java/com/kodilla/ecommerce/repository/OrderItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kodilla.ecommerce.repository;
-
-import com.kodilla.ecommerce.domain.OrderItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-}
--- src/main/java/com/kodilla/ecommerce/service/OrderItemService.java
+++
@@ -1,4 +0,0 @@
-package com.kodilla.ecommerce.service;
-
-public class OrderItemService {
-}
