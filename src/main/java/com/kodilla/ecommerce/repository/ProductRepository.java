cc57de2f7277c64f5cced0e4245570aaffaaf0be
Bartosz Natora <bartek@MacBook-Pro-Bartosz.local>
2021-07-08 00:03:59 +0200

    JDP210601-21_rev.1: comments improvements on real ProductController methods, added relations in Cart and Group entities

--- src/main/java/com/kodilla/ecommerce/repository/ProductRepository.java
+++ src/main/java/com/kodilla/ecommerce/repository/ProductRepository.java
@@ -1,7 +1,6 @@
 package com.kodilla.ecommerce.repository;
 
 import com.kodilla.ecommerce.domain.Product;
-<<<<<<< HEAD
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -16,21 +15,4 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     void deleteById(long id);
 
     boolean existsById(Long id);
-=======
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
-
-@Repository
-public interface ProductRepository extends CrudRepository<Product, Integer> {
-
-    List<Product> findAll();
-
-    Product findById(long id);
-
-    Product save(Product product);
-
-    void deleteById(long id);
->>>>>>> b1c4854 (JDP210601-21: real implementation of ProductController, added ProductMapper, ProductRepository, ProductService, added @Builder annotation in Product)
 }
