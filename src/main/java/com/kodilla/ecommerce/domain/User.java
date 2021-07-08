2067f1bb089d268f5127b077d7ef3edf0d1828c0
Michal Swigost <mike@data.pl>
2021-07-08 13:49:38 +0200

    JDP210601-15: entity edit

--- src/main/java/com/kodilla/ecommerce/domain/User.java
+++ src/main/java/com/kodilla/ecommerce/domain/User.java
@@ -3,20 +3,30 @@ package com.kodilla.ecommerce.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "Users")
+@Table(name = "USERS")
+@Access(AccessType.FIELD)
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ID")
     private Long id;
+    @Column(name = "USER_NAME")
+    private String name;
+    @Column(name = "USER_EMAIL")
     private String email;
+    @Column(name = "USER_ADDRESS")
     private String address;
+    @Column(name = "USER_LOGIN")
     private String userLogin;
+    @Column(name = "USER_STATUS")
     private boolean userStatus;
+    @Column(name = "USER_PASSWORD")
     private String password;
 }
