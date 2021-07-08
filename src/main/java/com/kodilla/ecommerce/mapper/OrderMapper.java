4112c04e0a4435e54f2ebe980f625dcaf08935dd
piotrek-kuder <kuder.piotr@o2.pl>
2021-07-08 10:01:05 +0200

    Jdp210601-24 adding OrderMapper

---
+++ src/main/java/com/kodilla/ecommerce/mapper/OrderMapper.java
@@ -0,0 +1,50 @@
+package com.kodilla.ecommerce.mapper;
+
+import com.kodilla.ecommerce.domain.Order;
+import com.kodilla.ecommerce.domain.OrderItem;
+import com.kodilla.ecommerce.domain.User;
+import com.kodilla.ecommerce.dto.OrderDto;
+import com.kodilla.ecommerce.dto.OrderItemDto;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Component
+public class OrderMapper {
+
+    OrderItemMapper orderItemMapper;
+
+    public OrderDto mapToOrderDto(Order order) {
+
+        List<OrderItem> orderItems = order.getOrderItems();
+        //order.setOrderDate(LocalDate.parse("2021-07-08"));
+
+        return OrderDto.builder()
+                .id(order.getId())
+                .userId(order.getUserId())
+                .cartId(order.getCartId())
+                .orderStatus(order.getOrderStatus())
+                .totalPrice(order.getTotalPrice())
+                .orderDate(order.getOrderDate().toString())
+                .orderItemDtos(orderItemMapper.mapToOrderItemDtoList(orderItems))
+                .build();
+    }
+
+
+    public Order mapToOrder(OrderDto orderDto) {
+
+        List<OrderItemDto> orderItemDtos = orderDto.getOrderItemDtos();
+
+        return new Order(
+                orderDto.getId(),
+                orderDto.getUserId(),
+                orderDto.getCartId(),
+                orderDto.getOrderStatus(),
+                orderDto.getTotalPrice(),
+                LocalDate.parse(orderDto.getOrderDate()),
+                orderItemMapper.mapToOrderItemList(orderItemDtos),
+                new User() // userService.getUser(orderDto.getUserId())
+        );
+    }
+}
