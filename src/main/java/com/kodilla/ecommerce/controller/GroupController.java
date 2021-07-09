207f3663f155c4311612d1ceac15b5cc4dcc96fe
mc139 <maciejandrzejcieslik@gmail.com>
2021-07-09 14:15:25 +0100

    final version of Group Controller

--- src/main/java/com/kodilla/ecommerce/controller/GroupController.java
+++ src/main/java/com/kodilla/ecommerce/controller/GroupController.java
@@ -21,68 +21,41 @@ public class GroupController {
 
     private GroupService groupService;
 
-        @RequestMapping(method = RequestMethod.GET, value = "getGroups")
+
+    ///////////////////////////////////////////////////////////////////////
+    /*
+    Ponizej znajduje się skrócona implementacja tych samych endpointów mógłby mi ktos powiedziec dla czego nie działa ?
+    */
+    @GetMapping
     public List<GroupDto> getGroups() {
-            log.info("{}","get groups");
-            return new ArrayList<>(List.of(new GroupDto(1,"test get groups")));
+        ArrayList<GroupDto> testGetGroups = new ArrayList<>(List.of(new GroupDto(1, "testGetGroups")));
+        log.info("{}", testGetGroups);
+        return testGetGroups;
 //        return groupService.getAllGroups();
     }
 
     @GetMapping("/{id}")
     public GroupDto getGroup(@PathVariable long id) {
-            log.info("{}" , "test group");
-            return new GroupDto(2,"test Get group");
+        GroupDto test_getGroup = new GroupDto(2, "test getGroup");
+        log.info("{}", test_getGroup);
+        return test_getGroup;
 //        return groupService.getGroupById(id);
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "create",
+    @PostMapping(
             consumes = MediaType.APPLICATION_JSON_VALUE)
     public void createGroup(@RequestBody GroupDto groupDto) {
+        log.info("{}", groupDto);
 //        groupService.createGroup(groupDto);
     }
 
-    @RequestMapping(method = RequestMethod.PUT, value = "update",
+    @PatchMapping(
             consumes = MediaType.APPLICATION_JSON_VALUE)
     public GroupDto updateGroup(@RequestBody GroupDto groupDto) {
-
-        return new GroupDto(5,"test update group");
+        GroupDto test_updateGroup = new GroupDto(1, "Test updateGroup");
+        log.info("{}", groupDto);
+        return test_updateGroup;
 //        return groupService.updateGroup(groupDto);
     }
-///////////////////////////////////////////////////////////////////////
-    /*
-    Ponizej znajduje się skrócona implementacja tych samych endpointów mógłby mi ktos powiedziec dla czego nie działa ?
-    */
-//    @GetMapping("/{getGroups}")
-//    public List<GroupDto> getGroups()
-//    {
-//        ArrayList<GroupDto> testGetGroups = new ArrayList<>(List.of(new GroupDto(1, "testGetGroups")));
-//        log.info("{}",testGetGroups);
-//        return testGetGroups;
-////        return groupService.getAllGroups();
-//    }
-
-//    @GetMapping("/{id}")
-//    public GroupDto getGroup(@PathVariable long id) {
-//        GroupDto test_getGroup = new GroupDto(2, "test getGroup");
-//        log.info("{}",test_getGroup);
-//        return test_getGroup;
-////        return groupService.getGroupById(id);
-//    }
-//
-//    @PostMapping(
-//            consumes = MediaType.APPLICATION_JSON_VALUE)
-//    public void createGroup(@RequestBody GroupDto groupDto) {
-//        log.info("{}",groupDto);
-////        groupService.createGroup(groupDto);
-//    }
-//
-//    @PatchMapping( value = "/{update}",
-//            consumes = MediaType.APPLICATION_JSON_VALUE)
-//    public GroupDto updateGroup(@RequestBody GroupDto groupDto) {
-//        GroupDto test_updateGroup = new GroupDto(1, "Test updateGroup");
-//        log.info("{}", groupDto);
-//        return test_updateGroup;
-////        return groupService.updateGroup(groupDto);
-//    }
 
 }
