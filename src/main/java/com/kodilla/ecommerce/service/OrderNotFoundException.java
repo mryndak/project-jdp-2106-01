3e19bfe79b73c894214d8f6a1cc889b3dc03f2cb
piotrek-kuder <kuder.piotr@o2.pl>
2021-07-10 21:41:32 +0200

    JDP210601-24_real_impl fixing OrderNotFoundException

--- src/main/java/com/kodilla/ecommerce/service/OrderNotFoundException.java
+++ src/main/java/com/kodilla/ecommerce/service/OrderNotFoundException.java
@@ -1,4 +1,8 @@
 package com.kodilla.ecommerce.service;
 
 public class OrderNotFoundException extends Exception {
+    @Override
+    public String getMessage() {
+        return "com.kodilla.ecommerce.service.OrderNotFoundException: no order with given ID";
+    }
 }
