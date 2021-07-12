6909703f1012551a07570024eb73417b1d165ff3
mc139 <maciejandrzejcieslik@gmail.com>
2021-07-12 14:07:16 +0100

    Group Entity fixed

--- src/main/java/com/kodilla/ecommerce/domain/Group.java
+++ src/main/java/com/kodilla/ecommerce/domain/Group.java
@@ -1,44 +1,36 @@
 package com.kodilla.ecommerce.domain;
 
+import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-@Entity
-@Table(name = "GROUP")
+@Table
+@Entity(name = "PRODUCT_GROUPS")
 public class Group {
-
-    private long id;
-    private String name;
-//    private List<Product> products;
-
-//    @OneToMany(
-//            targetEntity = Product.class,
-//            mappedBy = "group",
-//            cascade = CascadeType.ALL,
-//            fetch = FetchType.LAZY
-//    )
-//    public List<Product> getProducts() {
-//        return products;
-//    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     @Id
     @GeneratedValue
-    public long getId() {
-        return id;
-    }
+    @Column
+    private long id;
+
+    private String name;
+
+    @OneToMany(
+            targetEntity = Product.class,
+            mappedBy = "group",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    @Column(name = "PRODUCTS")
+    private final List<Product> productList = new ArrayList<>();
 
 }
