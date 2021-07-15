75b0c7be6a033423cc5340d193941e25d5caecf8
Michal Swigost <mike@data.pl>
2021-07-15 09:31:59 +0200

    JDP210601-20 created test Class

---
+++ src/test/java/com/kodilla/ecommerce/entities/UserEntityTest.java
@@ -0,0 +1,9 @@
+package com.kodilla.ecommerce.entities;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+public class UserEntityTest {
+
+}
